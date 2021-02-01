5406a46f00095e8772b29788529a77ff21cd93a7
Ankit <ankitrawat7895@gmail.com>
2021-02-01 14:52:08 -0800

    Add files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let myform = document.getElementById('myform');
 let targetimage = document.getElementById('targetimage');
 let inputrange = document.querySelectorAll('.slider');
+myform.addEventListener('submit', function(e){
+    let urlimage = document.getElementById('onlineurl');
+    let urlimageval = urlimage.value;
+    if(urlimageval.length){
